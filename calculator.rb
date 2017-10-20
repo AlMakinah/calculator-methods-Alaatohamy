77abc8bfa0f87983d75c8ceaad8a37ddc93d185e
alaa <alaatohamy4@gmail.com>
2017-10-20 18:41:50 +0200

    modify power method

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
  #!/home/alaa/.rbenv/shims/ruby
  def getoprands
-    puts 'Enter 2 numbers and the operation,Please'
+    puts 'Enter 2 numbers then the operation,Please'
     op1 = gets.chomp.to_i
     op2 = gets.chomp.to_i
     operation = gets.chomp
@@ -20,33 +20,35 @@
 
   def sumition op1, op2
     sum = op1 + op2
-    puts sum
+    return sum
   end
 
   def subtraction op1, op2
     sub = op1 - op2
-    puts sub
+    return sub
   end
 
   def Multiplication op1, op2
     multip = op1 * op2
-    puts multip
+    return multip
   end
 
   def Divide op1, op2
     div = op1 / op2
-    puts div
+    return div
   end
   def power op1, op2
     n = 1
-    while n <= op2
-      pow = op1 * op1
+    pow = op1
+    return 0 if op2 == 0
+    while n < op2
+      pow *= op1
       n +=1
     end
-    puts pow
+     return pow
   end
 
-  getoprands       
+  puts getoprands       
    
 
  # def calculate (op1, op2, operation)
